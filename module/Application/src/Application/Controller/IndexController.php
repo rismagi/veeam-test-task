a31cae449751e3a036b171246e6faca7b4010ae9
Renat Ismagilov <rismagi@gmail.com>
2014-03-15 22:31:22 +0400

    Vacancies list

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -11,11 +11,42 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Application\Entity\Vacancy;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        $objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+
+        $vacancies = $objectManager
+            ->getRepository('\Application\Entity\Vacancy')
+            ->findAll();
+
+        $view = new ViewModel(array(
+            'items' => $vacancies,
+        ));
+
+        return $view;
+    }
+
+    public function addAction()
+    {
+        $objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        $repository = $objectManager->getRepository('Gedmo\Translatable\Entity\Translation');
+
+        for($n = 1; $n < 10; $n++) {
+            $item = new Vacancy();
+            $item->setName('Vacancy EN ' . $n);
+            $item->setDescription('Vacancy EN description ' . $n);
+
+            $repository->translate($item, 'name', 'de_DE', 'Vacancy DE ' . $n)
+                ->translate($item, 'description', 'de_DE', 'Vacancy DE description ' . $n)
+                ->translate($item, 'name', 'ru_RU', 'Вакансия RU ' . $n)
+                ->translate($item, 'description', 'ru_RU', 'Описание RU вакансии ' . $n);
+
+            $objectManager->persist($item);
+        }
+        $objectManager->flush();
     }
 }
---
+++ module/Application/src/Application/Entity/Vacancy.php
@@ -0,0 +1,86 @@
+<?php
+namespace Application\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+/**
+ * @ORM\Entity
+ */
+class Vacancy
+{
+    /**
+     * @Gedmo\Locale
+     * Used locale to override Translation listener`s locale
+     * this is not a mapped field of entity metadata, just a simple property
+     */
+    private $locale;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @Gedmo\Translatable
+     * @ORM\Column(type="string", length=128)
+     */
+    protected $name;
+
+    /**
+     * @Gedmo\Translatable
+     * @ORM\Column(type="string")
+     */
+    protected $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Department")
+     */
+    protected $department;
+
+    /**
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime",nullable=true)
+     */
+    protected $updated;
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $locale
+     */
+    public function setTranslatableLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+}
